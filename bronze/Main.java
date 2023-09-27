dc68900766cb4ae44fe221044e10a0e296320326
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-09-28 01:28:35 +0900

    27866번 문자와 문자열 브론즈5

--- bronze/Main.java
+++ bronze/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-
         String S = scan.next();
         int i = scan.nextInt();
         System.out.println(S.charAt(i-1));
