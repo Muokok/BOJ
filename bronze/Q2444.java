01f359171e2c9a73ccf0a539374e151610cdad80
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-10-09 23:15:25 +0900

    2444번/별 찍기-7/브론즈3 틀렸음

--- bronze/Q2444.java
+++ bronze/Q2444.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 public class Q2444 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         int num = scan.nextInt();
-
         // Upper part of the pattern
         for (int i = 1; i <= num; i++) {
             // Print leading spaces
