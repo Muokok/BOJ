c4fbf0789497f97159792479713293e24ed5c108
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-10-18 23:51:53 +0900

    2738/행렬 덧셈/브론즈5

---
+++ bronze/Q2738.java
@@ -0,0 +1,40 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Q2738 {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int M = scan.nextInt();
+
+
+        int [][] arr1 = new int[N][M];
+        int [][] arr2 = new int[N][M];
+
+        int [][] result = new int[N][M];
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                arr1[i][j] = scan.nextInt();
+            }
+        }
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                arr2[i][j] = scan.nextInt();
+            }
+        }
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                System.out.print(arr1[i][j] + arr2[i][j]+ " ");
+                if(j == M-1)
+                    System.out.println();
+            }
+        }
+
+
+    }
+}
