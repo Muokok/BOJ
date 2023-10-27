67d454293904dd55ef16419fbae3258d972c98eb
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-10-27 20:16:20 +0900

    2903/중앙 이동 알고리즘/브론즈3 알고리즘 구글링함

---
+++ bronze/Q2903.java
@@ -0,0 +1,25 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Q2903 {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int initial = 2;
+        int multiple = 0;
+        //3번은 81개 4 > 9 > 25 > 81
+
+        // 답 = 한 변의 점 개수의 제곱
+        // 한 변의 점의 개수가 증가하는 루틴이 1,2,4,8,16 즉 2^n
+
+        for(int i=0; i<N;i++){
+
+            multiple = multiple + (int)Math.pow(2,i);
+        }
+
+        System.out.println((int)Math.pow(multiple+initial,2));
+
+    }
+}
