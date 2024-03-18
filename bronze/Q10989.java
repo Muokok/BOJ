1133e044a8330e1d2a454966c2c62f4016bef331
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-03-18 16:24:39 +0900

    Q10989/수 정렬하기3/브론즈1

--- bronze/Q10989.java
+++ bronze/Q10989.java
@@ -1,26 +1,25 @@
-package silver;
+package bronze;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class Q2751 {
+public class Q10989 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer> list = new ArrayList<>();
+        int [] list = new int[N];
 
         for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-
-            list.add(Integer.parseInt(line));
+            list[i] = Integer.parseInt(br.readLine());
         }
 
+        Arrays.sort(list);
 
-        Collections.sort(list);
         for(int value : list) {
             sb.append(value).append('\n');
         }
