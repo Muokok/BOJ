271cc6f3ad67be508df6806cafc8f1a66f3806f2
Muokok <sgo5178@kyonggi.ac.kr>
2024-12-31 12:30:17 +0900

    인프런 문제/버블 정렬/답은 맞는데 정렬 방식이 달랐음

--- inflearn/section7/버블_정렬.java
+++ inflearn/section7/버블_정렬.java
@@ -16,12 +16,12 @@ public class 버블_정렬 {
             arr [k] = Integer.parseInt(input[k]);
         }
 
-        for(int i =0; i<N; i++){
-            for(int j=i+1; j<N; j++){
-                if(arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        for(int i =0; i<N - 1; i++){
+            for(int j=0; j<N -i -1; j++){
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
