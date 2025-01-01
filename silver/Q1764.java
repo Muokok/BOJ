1580502b9b87f48d3d6874399cbd4322cae4293f
Muokok <sgo5178@kyonggi.ac.kr>
2025-01-01 20:41:17 +0900

    백준/해시/개선된 버전 추가

--- silver/Q1764.java
+++ silver/Q1764.java
@@ -4,10 +4,38 @@ import java.util.*;
 
 public class Q1764 {
 
-    public static void main(String [] args){
+//    public static void main(String [] args){
+//        Scanner sc = new Scanner(System.in);
+//        Set<String> set = new HashSet<>();
+//        List<String> list = new ArrayList<>();
+//
+//        int N = sc.nextInt();
+//        int M = sc.nextInt();
+//
+//        for(int i =0; i<N; i++){
+//            String name = sc.next();
+//            set.add(name);
+//        }
+//
+//        for(int j=0; j<M; j++){
+//            String name = sc.next();
+//            if(set.contains(name)){
+//                list.add(name);
+//            }
+//        }
+//
+//        Collections.sort(list);
+//
+//        System.out.print(list.size() + "\n");
+//        for(String name : list){
+//            System.out.print(name + "\n");
+//        }
+//    }
+
+        public static void main(String [] args){ // 자동 정렬되는 트리셋을 사용함.
         Scanner sc = new Scanner(System.in);
         Set<String> set = new HashSet<>();
-        List<String> list = new ArrayList<>();
+        Set<String> result = new TreeSet<>();
 
         int N = sc.nextInt();
         int M = sc.nextInt();
@@ -20,15 +48,15 @@ public class Q1764 {
         for(int j=0; j<M; j++){
             String name = sc.next();
             if(set.contains(name)){
-                list.add(name);
+                result.add(name);
             }
         }
 
-        Collections.sort(list);
 
-        System.out.print(list.size() + "\n");
-        for(String name : list){
+        System.out.print(result.size() + "\n");
+        for(String name : result){
             System.out.print(name + "\n");
         }
     }
+
 }
