0b0c583b02d69e8470b7b36d03fa83705a3e1123
Muokok <sgo5178@kyonggi.ac.kr>
2025-01-03 14:16:48 +0900

    프로그래머스/???/성공, 개선 가능

---
+++ programmers/L1_두_정수_사이의_합.java
@@ -0,0 +1,24 @@
+package programmers;
+
+public class L1_두_정수_사이의_합 {
+
+//    public long solution(int a, int b) { // 나의 풀이
+//        long answer = 0;
+//
+//        int max = Math.max(a,b);
+//        int min = Math.min(a,b);
+//
+//        for(long i=min; i <=max; i++){
+//            answer += i;
+//        }
+//        return answer;
+//    }
+
+    public long solution(int a, int b) { // Perplexity가 개선해준 코드
+        long max = Math.max(a, b);
+        long min = Math.min(a, b);
+        return (max - min + 1) * (max + min) / 2;
+    }
+
+
+}
