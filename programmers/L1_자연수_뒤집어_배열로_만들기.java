4e299f5821232a7d0f9b495cf549466f810255c0
Muokok <sgo5178@kyonggi.ac.kr>
2025-01-06 13:19:23 +0900

    Lv.1/???/성공

---
+++ programmers/L1_자연수_뒤집어_배열로_만들기.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class L1_자연수_뒤집어_배열로_만들기 {
+
+    public int[] solution(long n) {
+        String stringN = n + "";
+
+        int[] answer = new int [stringN.length()];
+        int j = 0;
+
+        for(int i=stringN.length()-1; i>=0; i--){
+            answer[i] = Character.getNumericValue(stringN.charAt(j));
+            //answer[i] = Integer.parseInt(stringN.charAt(j)); 이건 오류가 남
+            j++;
+        }
+
+        return answer;
+    }
+}
