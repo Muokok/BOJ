c6e69189ad38f11c363c1648f81771614a05bfd2
Muokok <sgo5178@kyonggi.ac.kr>
2025-01-09 19:33:14 +0900

    이코테/dfs/실패_아예 접근법조차 모르겠음, 풀다가 포기

--- eecote/bfs/미로_탈출.java
+++ eecote/bfs/미로_탈출.java
@@ -9,12 +9,12 @@ public class 미로_탈출 {
     public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String [] nm = br.readLine();
+        String [] nm = br.readLine().split(" ");
 
         int n = Integer.parseInt(nm[0]);
         int m = Integer.parseInt(nm[1]);
 
-        for(int i=0; i<nl i++){
+        for(int i=0; i<nm.length; i++){
             String input = br.readLine();
             for(int j=0; j<m; j++){
                 graph[i][j] = input.charAt(j) - '0';
