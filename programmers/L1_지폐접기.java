3656f75c3f0b45df039515052799d196b607e37a
Muokok <sgo5178@kyonggi.ac.kr>
2025-01-11 17:08:28 +0900

    Lv.1/구현/성공

---
+++ programmers/L1_지폐접기.java
@@ -0,0 +1,25 @@
+package programmers;
+import java.util.*;
+
+public class L1_지폐접기 {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+
+        while(true){
+            Arrays.sort(wallet);
+            Arrays.sort(bill);
+
+            if(wallet[0] >= bill[0] && wallet[1] >= bill[1]){
+                break;
+            }else{
+                bill[1] /= 2;
+                answer ++;
+            }
+        }
+
+
+
+
+        return answer;
+    }
+}
