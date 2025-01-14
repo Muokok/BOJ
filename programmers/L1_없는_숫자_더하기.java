1a39bd620c8be8d47cb85f3fc33185526128dd28
Muokok <sgo5178@kyonggi.ac.kr>
2025-01-14 10:10:36 +0900

    Lv.1/해시/성공

---
+++ programmers/L1_없는_숫자_더하기.java
@@ -0,0 +1,27 @@
+package programmers;
+import java.util.*;
+
+public class L1_없는_숫자_더하기 {
+
+    public int solution(int[] numbers) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        int answer = 0;
+
+        for(int i=0; i<10; i++){
+            map.put(i,0);
+        }
+
+        for(int j=0; j<numbers.length; j++){
+            map.put(numbers[j], map.get(j) + 1);
+        }
+
+        for(int k=0; k<10; k++){
+            if(map.get(k) == 0){
+                answer += k;
+            }
+        }
+
+        return answer;
+    }
+}
