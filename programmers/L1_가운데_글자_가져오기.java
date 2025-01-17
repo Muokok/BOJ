75004f46026fdf13131f238305d75d488d9bbc39
Muokok <sgo5178@kyonggi.ac.kr>
2025-01-17 11:46:16 +0900

    Lv.1/구현/성공

---
+++ programmers/L1_가운데_글자_가져오기.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class L1_가운데_글자_가져오기 {
+
+    public String solution(String s) {
+
+        if(s.length() % 2 !=0){
+            return s.charAt((int) s.length() / 2) + "";
+        }else{
+            return s.charAt((int) ((s.length() / 2)  -1)) + "" + s.charAt((int) s.length() / 2);
+        }
+    }
+}
