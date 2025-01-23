502a60ac2020c88a86ee20e6fea1c5ec751876ff
Muokok <sgo5178@kyonggi.ac.kr>
2025-01-23 11:57:30 +0900

    실버3/DFS/성공

---
+++ Q2606.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Q2606 { // 바이러스
+    static ArrayList<Integer>[] graph;
+    static boolean [] visited;
+    static int cnt = 0;
+
+    static void dfs(int node){
+        visited[node] = true;
+
+        for(int next : graph[node]){
+            if(!visited[next]){
+                cnt++;
+                dfs(next);
+            }
+        }
+    }
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        graph = new ArrayList [n+1];
+        for(int i=1; i<= n; i++){
+            graph[i] = new ArrayList<Integer>();
+        }
+
+        visited = new boolean[n+1];
+
+        int networks = sc.nextInt();
+        for(int i=0; i<networks; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            graph[x].add(y);
+            graph[y].add(x);
+        }
+
+        dfs(1);
+
+        System.out.print(cnt);
+    }
+}
