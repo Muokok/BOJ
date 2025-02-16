b2f76fe341a462952512072173cac29b1d2c6c13
Muokok <sgo5178@kyonggi.ac.kr>
2025-02-16 12:09:09 +0900

    코드 개선 및 주석 추가

--- programmers/lv1/L1_택배_상자_꺼내기.java
+++ programmers/lv1/L1_택배_상자_꺼내기.java
@@ -39,24 +39,50 @@ public class L1_택배_상자_꺼내기 {
             }
         }
 
+        /**     위 코드를 아래처럼 개선할 수 있음.
+         *         // 몇 번째 층에 위치하는지
+         *         int layer = (num - 1) / w;
+         *
+         *         // 몇 번째 열(column)에 위치하는지 찾기
+         *         int indexInLayer = (num - 1) % w; // 0-based index
+         *
+         *         // 열(column) 찾기 (층이 짝수면 왼 -> 오, 홀수면 오 -> 왼)
+         *         int column;
+         *         if (layer % 2 == 0) {
+         *             column = indexInLayer; // 짝수 층: 왼 -> 오
+         *         } else {
+         *             column = w - 1 - indexInLayer; // 홀수 층: 오 -> 왼
+         *         }
+         * */
+
         // 꺼내야 하는 상자의 개수 계산
         int count = 0;
 
-        outerLoop:
-        for(int i=targetRow; i>=0; i--){
-
-            for(int j=0; j< w; j++){
-                if(warehouse[i][j] == num){
-                    if(warehouse[h-1][j] == 0){
-                        count = h -1 - (i+1) +1;
-                    }else{
-                        count = h - (i+1) +1;
-                    }
-                    break outerLoop;
+        for(int j=0; j< w; j++){
+            if(warehouse[targetRow][j] == num){
+                if(warehouse[h-1][j] == 0){
+                    count = h -1 - (targetRow+1) +1;
+                }else{
+                    count = h - (targetRow+1) +1;
                 }
+                break;
             }
-
         }
+
         return count;
+
+        /**        위 코드 아래처럼도 가능
+         *         // 위에 있는 상자 개수 계산
+         *         int count = 0;
+         *         for (int i = layer; i < (n + w - 1) / w; i++) {
+         *             if (i % 2 == 0) { // 짝수층: 왼 -> 오
+         *                 if (column < (n - i * w)) count++;
+         *             } else { // 홀수층: 오 -> 왼
+         *                 if ((w - 1 - column) < (n - i * w)) count++;
+         *             }
+         *         }
+         *
+         *         return count;
+         * */
     }
 }
